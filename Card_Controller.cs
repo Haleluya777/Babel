ea0419dca8d5ac8c703b7cbf6b69e1983d29598c
Honolulu <88095893+Haleluya777@users.noreply.github.com>
2022-04-24 20:40:56 +0900

    Add files via upload

---
+++ Card_Controller.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card_Controller : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public static Vector2 DefaultPos;
+    public static Card_Deck_Controller deck;
+    public GameObject Deck_Position;
+    RectTransform trans;
+    public void Awake()
+    {
+        deck = GameObject.Find("Deck_Pos").GetComponent<Card_Deck_Controller>();
+        trans = GetComponent<RectTransform>();
+        Deck_Position = GameObject.Find("Deck_Pos");
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.trans.anchoredPosition = new Vector2(trans.anchoredPosition.x, trans.anchoredPosition.y + 34);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.trans.anchoredPosition = new Vector2(trans.anchoredPosition.x, trans.anchoredPosition.y - 34);
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        DefaultPos = this.transform.position;
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Vector2 currentPos = Input.mousePosition;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        this.transform.position = currentPos;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        if (Input.mousePosition.y > 300)
+        {
+            //Card_Using();
+            this.gameObject.SetActive(false);
+            deck.size--;
+            this.transform.parent.gameObject.SetActive(false);
+            this.transform.SetParent(Deck_Position.transform);
+            this.transform.position = Deck_Position.transform.position;
+        }
+        else
+        {
+            this.transform.position = DefaultPos;
+            transform.rotation = Quaternion.Euler(0, 0, transform.parent.eulerAngles.z);
+        }
+    }
+
+    //카드 효과발동
+    //public void Card_Using()
+    //{
+    //    string tg = this.tag;
+    //    switch (tg)
+    //    {
+    //        case "Move_Right":
+    //            break;
+    //
+    //        case "Move_Left":
+    //            break;
+    //
+    //        case "Move_Up":
+    //            break;
+    //
+    //        case "Move_Down":
+    //            break;
+    //    }
+    //}
+}
---
+++ Card_Deck_Controller.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Deck_Controller : MonoBehaviour
+{
+    List<GameObject> cd_hand = new List<GameObject>();
+    RectTransform rect;
+    public GameObject[] deck = new GameObject[30];
+    public GameObject card_Pos;
+    public int size = 5;
+    public int deck_count = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        DeckSetting();
+        Start_Setting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Card_Pos_Rotate();
+    }
+
+    public void Start_Setting()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Draw();
+        }
+    }   
+
+    public void Draw()
+    {
+        int num = Random.Range(0,deck_count);
+        int chid_num = 0;
+        deck_count--;
+        while(true)
+        {
+            if (card_Pos.transform.GetChild(chid_num).gameObject.activeSelf == false)
+            {
+                card_Pos.transform.GetChild(chid_num).gameObject.SetActive(true);
+                break;
+            }
+            else chid_num++;   
+        }
+
+        GameObject rand_card = this.transform.GetChild(num).gameObject;
+        rand_card.transform.SetParent(card_Pos.transform.GetChild(chid_num));
+        rand_card.transform.position = card_Pos.transform.GetChild(chid_num).transform.GetChild(0).position;
+        rand_card.transform.rotation = card_Pos.transform.GetChild(chid_num).rotation;
+        rand_card.gameObject.SetActive(true);
+    }
+
+    public void Button_Draw()
+    {
+        if(size < 7)
+        {
+            size++;
+            Draw();
+        }
+    }
+
+    public void DeckSetting()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            //deck[i] = Resources.Load<GameObject>("Prefabs/Card");
+            Instantiate(deck[i], this.transform);
+            deck[i].gameObject.SetActive(false);
+            deck[i].transform.position = this.transform.position;
+        }
+    }
+
+    public void Card_Pos_Rotate()
+    {
+        float first_num = (11.25f * size - 1) + 12.25f;
+        for(int i = 0; i < 7; i++)
+        {
+            if(card_Pos.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                card_Pos.transform.GetChild(i).rotation = Quaternion.Euler(0, 0, first_num -= 22.5f);
+            }
+        }
+    }
+}
